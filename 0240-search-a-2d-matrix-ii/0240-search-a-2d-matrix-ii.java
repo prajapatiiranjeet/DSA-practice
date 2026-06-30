7a0e2227bc44f76db6a72e4f4abee6b233bc3ac1
prajapatiiranjeet <157957421+prajapatiiranjeet@users.noreply.github.com>
2026-06-30 22:14:10 +0530

    Time: 3 ms (88.69%), Space: 48.5 MB (10.25%) - LeetHub

---
+++ 0240-search-a-2d-matrix-ii/0240-search-a-2d-matrix-ii.java
@@ -0,0 +1,28 @@
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if (matrix == null || matrix.length == 0 || matrix[0].length == 0) {
+            return false;
+        }
+        
+        int m = matrix.length;
+        int n = matrix[0].length;
+        
+        // Start from top-right corner
+        int row = 0;
+        int col = n - 1;
+        
+        while (row < m && col >= 0) {
+            if (matrix[row][col] == target) {
+                return true;
+            } else if (matrix[row][col] > target) {
+                // Current value is too large, move left (smaller values)
+                col--;
+            } else {
+                // Current value is too small, move down (larger values)
+                row++;
+            }
+        }
+        
+        return false;
+    }
+}
