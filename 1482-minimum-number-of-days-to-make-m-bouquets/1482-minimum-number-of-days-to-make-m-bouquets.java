df78175be4969d05946cee2eeb37eee3681338bc
prajapatiiranjeet <157957421+prajapatiiranjeet@users.noreply.github.com>
2026-07-28 13:12:41 -0700

    Time: 19 ms (38.99%), Space: 82.8 MB (29.2%) - LeetHub

--- 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
+++ 1482-minimum-number-of-days-to-make-m-bouquets/1482-minimum-number-of-days-to-make-m-bouquets.java
@@ -1,62 +1,61 @@
 class Solution {
-
     static int findmin(int[] arr){
         int min = Integer.MAX_VALUE;
-        for(int x : arr){
-            min = Math.min(min, x);
+        for(int ans : arr){
+            min = Math.min(min , ans);
         }
         return min;
+
+
     }
 
     static int findmax(int[] arr){
-        int max = Integer.MIN_VALUE;
-        for(int x : arr){
-            max = Math.max(max, x);
+          int max = Integer.MIN_VALUE;
+
+           for(int ans : arr){
+            max = Math.max(max, ans);
         }
-        return max;
+          
+          return max;
     }
 
-    static int findday(int[] arr, int k, int day){
-        int count = 0;
-        int bouquets = 0;
 
-        for(int x : arr){
-            if(x <= day){
-                count++;
-                if(count == k){
-                    bouquets++;
+    static int findday(int[] arr , int m ,int k, int mid ){
+            int count = 0;
+            int ans = 0;
+            for(int art:arr){
+                if(art <= mid){
+                    count = count+1;
+                    if(count == k){
+                        ans = ans+1;
+                        count = 0;
+                    }
+
+                }else{
                     count = 0;
                 }
-            }else{
-                count = 0;
             }
-        }
-        return bouquets;
+            return ans;
     }
-
     public int minDays(int[] bloomDay, int m, int k) {
-
-        if((long)m * k > bloomDay.length)
-            return -1;
-
         int l = findmin(bloomDay);
         int h = findmax(bloomDay);
-
         int ans = -1;
 
-        while(l <= h){
-            int mid = l + (h - l) / 2;
-
-            int bouquets = findday(bloomDay, k, mid);
-
-            if(bouquets >= m){
-                ans = mid;
-                h = mid - 1;
-            }else{
-                l = mid + 1;
-            }
+        if((long)m*k > bloomDay.length){
+            return -1;
         }
 
+        while(l<=h){
+            int mid = l+(h-l)/2;
+            int getans = findday(bloomDay , m , k , mid);
+           if(getans >= m){
+               ans = mid;
+               h = mid - 1;
+              }else{
+               l = mid + 1;
+           }
+        }
         return ans;
     }
 }
